21b062a88c815afe87eca92366151a4e215e9f6c
David Bento <dbento.dev@gmail.com>
2025-07-12 19:05:27 -0300

    wip: teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Typography } from '@mui/material'
+import { Typography, useTheme } from '@mui/material'
 import { lazy, Suspense } from 'react'
 
 const RemoteHeader = lazy(() => import('app-header/Header'))
 
 export const App = () => {
+  const theme = useTheme()
+
   return (
     <>
       <Suspense fallback={<div>Carregando header...</div>}>
         <RemoteHeader />
       </Suspense>
 
-      <Typography variant="h4" color="primary">
+      <Typography variant="h4" color={theme.palette.secondary.dark}>
         Teste de Estilo Global
       </Typography>
     </>
