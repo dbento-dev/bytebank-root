dd4cd1afd223b386afba409b5e575593f5aeaa02
David Bento <dbento.dev@gmail.com>
2025-08-01 17:08:48 -0300

    fix: remove erro boundary

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,26 +1,26 @@
 import './styles/style.css'
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import ErrorBoundary from './components/ErrorBoundary'
+// import ErrorBoundary from './components/ErrorBoundary'
 import { App } from './App'
 
 const RemoteThemeProvider = React.lazy(() => import('utilUi/ThemeProvider'))
 
-const AppWithoutTheme = () => {
-  console.warn(
-    "Falha ao carregar o ThemeProvider do MFE 'utilUi'. A aplicação será renderizada sem o tema customizado."
-  )
-  return <App />
-}
+// const AppWithoutTheme = () => {
+//   console.warn(
+//     "Falha ao carregar o ThemeProvider do MFE 'utilUi'. A aplicação será renderizada sem o tema customizado."
+//   )
+//   return <App />
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<AppWithoutTheme />}>
-      <Suspense fallback={<></>}>
-        <RemoteThemeProvider>
-          <App />
-        </RemoteThemeProvider>
-      </Suspense>
-    </ErrorBoundary>
+    {/* <ErrorBoundary fallback={<AppWithoutTheme />}> */}
+    <Suspense fallback={<></>}>
+      <RemoteThemeProvider>
+        <App />
+      </RemoteThemeProvider>
+    </Suspense>
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 )
